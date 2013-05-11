b8d5f0b1d518fa17d847487120a7a9b1d7f7b9b0
Anthony VEREZ <netantho@gmail.com>
2013-05-11 19:32:04 +0200

    rst module renammed to parser_rst to avoid name conflict with docutils' rst module

--- tests/test_rst.py
+++ tests/test_rst.py
@@ -6,18 +6,18 @@ import pytest
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "../src/epicslide"))
 
-import rst
+import parser_rst
 
 
-class TestRst(object):
-    def test_pygments(self):
-        p = rst.Pygments('')
-        print p.__dict__
-        print p.run()
-        assert False
+# class TestRst(object):
+#     def test_pygments(self):
+#         p = rst.Pygments('')
+#         print p.__dict__
+#         print p.run()
+#         assert False
 
-    def html_parts(self):
-        assert False
+#     def html_parts(self):
+#         assert False
     
-    def html_body(self):
-        assert False
+#     def html_body(self):
+#         assert False
